ec51dd314de63ecca290c018104938081cbc50a6
Darren Michael Puigh <darren.michael.puigh@cern.ch>
2011-10-20 01:33:48 +0000

    change ndof from int to double

--- interface/BNprimaryvertex.h
+++ interface/BNprimaryvertex.h
@@ -7,10 +7,10 @@
 // a simple class
 struct BNprimaryvertex
 {
-  explicit BNprimaryvertex(double v,int c):x(v),xError(v),y(v),yError(v),z(v),zError(v),rho(v),normalizedChi2(v),isFake(c),isValid(c),tracksSize(c),ndof(c) { }
-BNprimaryvertex():x(-99),xError(-99),y(-99),yError(-99),z(-99),zError(-99),rho(-99),normalizedChi2(-99),isFake(-99),isValid(-99),tracksSize(-99),ndof(-99) { }
-  double x,xError,y,yError,z,zError,rho,normalizedChi2;
-  int isFake,isValid,tracksSize,ndof;
+  explicit BNprimaryvertex(double v,int c):x(v),xError(v),y(v),yError(v),z(v),zError(v),rho(v),normalizedChi2(v),ndof(v),isFake(c),isValid(c),tracksSize(c),isGood(c) { }
+BNprimaryvertex():x(-99),xError(-99),y(-99),yError(-99),z(-99),zError(-99),rho(-99),normalizedChi2(-99),ndof(-99),isFake(-99),isValid(-99),tracksSize(-99),isGood(-99) { }
+  double x,xError,y,yError,z,zError,rho,normalizedChi2,ndof;
+  int isFake,isValid,tracksSize,isGood;
 
 };
 
