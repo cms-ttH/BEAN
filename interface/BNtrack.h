0ced44ac8de20fca40252f9362cfb3f84ea3b962
Darren Michael Puigh <darren.michael.puigh@cern.ch>
2012-10-08 03:41:08 +0000

    include information for disappearing tracks analysis

--- interface/BNtrack.h
+++ interface/BNtrack.h
@@ -11,6 +11,14 @@ struct BNtrack
   BNtrack():pt(-99),px(-99),py(-99),pz(-99),phi(-99),eta(-99),theta(-99),normChi2(-99),dZ(-99),d0(-99),d0err(-99),vx(-99),vy(-99),vz(-99),charge(-99),numValidHits(-99) { }
   double pt,px,py,pz,phi,eta,theta,normChi2,dZ,d0,d0err,vx,vy,vz;
   int    charge,numValidHits;
+
+  std::vector<int> subDetIdHits;
+  std::vector<int> rawDetIdHits;
+  std::vector<bool> isValidHits;
+  std::vector<double> modulePerpHits;
+  std::vector<double> moduleZHits;
+  std::vector<double> modulePhiHits;
+
 };
 
 // this is our new product, it is simply a 
