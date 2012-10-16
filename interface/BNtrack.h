ddf9ffb16c7004562f5352367cd9e6e8e35fc17e
Darren Michael Puigh <darren.michael.puigh@cern.ch>
2012-10-16 19:31:43 +0000

    include purity var

--- interface/BNtrack.h
+++ interface/BNtrack.h
@@ -7,10 +7,10 @@
 // a simple class
 struct BNtrack
 {
-  explicit BNtrack(double v, int c):pt(v),px(v),py(v),pz(v),phi(v),eta(v),theta(v),normChi2(v),dZ(v),d0(v),d0err(v),vx(v),vy(v),vz(v),charge(c),numValidHits(c) { }
-  BNtrack():pt(-99),px(-99),py(-99),pz(-99),phi(-99),eta(-99),theta(-99),normChi2(-99),dZ(-99),d0(-99),d0err(-99),vx(-99),vy(-99),vz(-99),charge(-99),numValidHits(-99) { }
+  explicit BNtrack(double v, int c):pt(v),px(v),py(v),pz(v),phi(v),eta(v),theta(v),normChi2(v),dZ(v),d0(v),d0err(v),vx(v),vy(v),vz(v),charge(c),numValidHits(c),isHighPurity(c) { }
+  BNtrack():pt(-99),px(-99),py(-99),pz(-99),phi(-99),eta(-99),theta(-99),normChi2(-99),dZ(-99),d0(-99),d0err(-99),vx(-99),vy(-99),vz(-99),charge(-99),numValidHits(-99),isHighPurity(-99) { }
   double pt,px,py,pz,phi,eta,theta,normChi2,dZ,d0,d0err,vx,vy,vz;
-  int    charge,numValidHits;
+  int    charge,numValidHits,isHighPurity;
 
   std::vector<int> subDetIdHits;
   std::vector<int> rawDetIdHits;
